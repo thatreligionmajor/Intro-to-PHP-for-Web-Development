9e12ec980f3a146c093da3d182b0566252559f3d
Theresa Barkasy <theresa.barkasy@gmail.com>
2023-12-06 13:34:20 -0500

    edited body text

--- homepagebody.php
+++ homepagebody.php
@@ -1,7 +1,7 @@
 <div class="container">
         <center>
           <?php
-            echo "this is the body";
+            echo "Strum into the heart of music with [Your Name], your dedicated banjo guru. Whether you're a beginner itching to pick up the banjo for the first time or a seasoned player looking to enhance your skills, you've landed in the right spot.";
           ?>
         </center>
 </div>
