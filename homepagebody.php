81791a9b63f911e01ddff5b094626650511e328e
Theresa Barkasy <theresa.barkasy@gmail.com>
2023-12-06 13:40:25 -0500

    added $your_name

--- homepagebody.php
+++ homepagebody.php
@@ -1,7 +1,13 @@
-<div class="container">
-        <center>
-          <?php
-            echo "Strum into the heart of music with [Your Name], your dedicated banjo guru. Whether you're a beginner itching to pick up the banjo for the first time or a seasoned player looking to enhance your skills, you've landed in the right spot.";
-          ?>
-        </center>
-</div>
+<?php
+  //to include things in your website from other files
+  include("variables.php");
+?>
+<body>
+  <div class="container">
+          <center>
+            <?php
+              echo "Strum into the heart of music with " . $your_name . ", your dedicated banjo guru. Whether you're a beginner itching to pick up the banjo for the first time or a seasoned player looking to enhance your skills, you've landed in the right spot.";
+            ?>
+          </center>
+  </div>
+</body>
