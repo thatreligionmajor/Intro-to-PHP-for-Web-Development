0ab9efa26791bd712fb0588a639b9788431f411e
Theresa Barkasy <theresa.barkasy@gmail.com>
2023-12-06 13:56:59 -0500

    added card

---
+++ card.php
@@ -0,0 +1,16 @@
+<?php
+  //to include things in your website from other files
+  include("variables.php");
+  ?>
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+            <?php
+                echo "Strum into the heart of music with " . $your_name . ", your dedicated banjo guru. Whether you're a beginner itching to pick up the banjo for the first time or a seasoned player looking to enhance your skills, you've landed in the right spot.";
+                ?>
+        </p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
