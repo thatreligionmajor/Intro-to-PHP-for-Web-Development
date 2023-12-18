bfc223b9e40da3cfe79adb5b5ee4150a0fdea689
Theresa Barkasy <theresa.barkasy@gmail.com>
2023-12-18 15:26:46 -0500

    added links in index

--- index.php
+++ index.php
@@ -12,14 +12,24 @@
       <link
         rel="stylesheet"
         type="text/css"
+        href="./styles/navbar.css"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
         href="./styles/jumbotron.css"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="./styles/footer.css"
       />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
       <?php
         //to include things in your website from other files
-        include($_SERVER['DOCUMENT_ROOT'] . '/Intro-to-PHP-for-Web-Development/variables.php');
+        include($_SERVER['DOCUMENT_ROOT'] . '/Intro-to-PHP-for-Web-Development/variables.php'); 
         include($_SERVER['DOCUMENT_ROOT'] . '/Intro-to-PHP-for-Web-Development/components/navbar.php');
         include($_SERVER['DOCUMENT_ROOT'] . '/Intro-to-PHP-for-Web-Development/components/jumbotron.php');
         
